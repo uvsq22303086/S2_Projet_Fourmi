9122f2bf3a591676189c4aab8f5df18aad92c0a3
AnnaNeste21 <161447705+AnnaNeste21@users.noreply.github.com>
2024-03-18 22:23:33 +0100

    Update Projet fourmi.py

--- Projet fourmi.py
+++ Projet fourmi.py
@@ -80,8 +80,14 @@ grille.mainloop() # Lancement de la boucle principale
 
 
 
-
-
+#fourmis : le code est inspiré du cite : https://rosettacode.org/wiki/Langton%27s_ant#Python
+def fourmis(width, height, max_nb_steps):
+    grid = [[" "] * width for _ in range(height)]
+    x = width // 2
+    y = height // 2
+    direction = "haut"
+i = 0
+    while i <
 
 
 
